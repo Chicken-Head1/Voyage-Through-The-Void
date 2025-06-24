dfc0642902c07f7d749e439754fc7aeccd292a2b
Elia <84045627+Chicken-Head1@users.noreply.github.com>
2025-06-24 16:46:23 +1000

    Update updater.py

--- files/updater.py
+++ files/updater.py
@@ -2,34 +2,64 @@
 
 import requests
 import os
-from icecream import ic
+import subprocess
+import sys
+import pygame
+import time
+
+
+
+pygame.init()
+WINDOW = pygame.display.set_mode(flags=pygame.RESIZABLE)
+pygame.display.set_caption("Space Simulator Updater")
+win_height = WINDOW.get_height()
+win_centrex = WINDOW.get_width()/2
+win_centrey = WINDOW.get_height()/2
+
+
+DOWNLOADING_FONT = pygame.font.SysFont(name='Courier', size=int(win_height/10.9))
+
+
+WINDOW.fill((0, 0, 0))
+
+downloading_text = DOWNLOADING_FONT.render("Updating game...", True, (255, 255, 255))
+downloading_text_rect = downloading_text.get_rect()
+WINDOW.blit(downloading_text, 
+            (
+                win_centrex-downloading_text_rect.width/2, # Xpos
+                win_centrey, # Ypos
+            ),
+            )
+
+pygame.display.flip()
 
 
 NEW_DATA_URL = "https://chicken-head1.github.io/Space-Simulator/files/update/new_data.txt"
 BASE_FILE_URL = "https://chicken-head1.github.io/Space-Simulator/files"
 DIRPATH: str = os.path.dirname(os.path.abspath(__file__))
 
-data = requests.get(url=NEW_DATA_URL)
-data = str(data.content).removeprefix("b'").removesuffix("\\n'")
+try:
+    data = requests.get(url=NEW_DATA_URL)
+    data = str(data.content).removeprefix("b'").removesuffix("\\n'")
+    data_list = data.split(";")
+    data_list = [i.split(":") for i in data_list]
 
-data_list = data.split(";")
-data_list = [i.split(":") for i in data_list]
-
+except requests.exceptions.SSLError:
+    print("Could not access webserver... unable to update game")
+    data_list = []
 
 for command_set in data_list:
-    command = command_set[0]
-    file = command_set[1]
+    try:
+        command = command_set[0]
+        file = command_set[1]
+    except IndexError: # No commands for the updater to use
+        break # Just reopen the game, this only occurs during development :3
     url = BASE_FILE_URL+file.replace('"', '')
     new_data = str(requests.get(url).content).removeprefix("b'").removesuffix("\\n'")
-    file_path = file.removeprefix('"').removesuffix('"')
-    file_path = DIRPATH+file_path
-
-    ic(url)
-    ic(new_data)
-
+    file = file.removeprefix('"').removesuffix('"')
+    file_path = DIRPATH+file
 
     if command == 'add': # add directory
-        file = file.replace('"','')
         file = file.removeprefix('/')
         try:
             os.mkdir(file)
@@ -40,12 +70,18 @@ for command_set in data_list:
         with open(file_path, 'w') as file:
             file.write(new_data)
     
-    elif command == 'rem': # remove file
+    elif command == 'rmf': # remove file)
         try:
-            os.remove(file)
+            os.remove(file_path)
         except: # suppress errors from exiting the script
             ...
-
+    
+    elif command == 'rmd': # remove directory
+        file = file.removeprefix('/')
+        try:
+            os.rmdir(file)
+        except: # suppress errors from exiting the script
+            ...
 
 def get_file_data(url: str) -> str:
     """
@@ -55,3 +91,24 @@ def get_file_data(url: str) -> str:
     data = str(requests.get(url).content).removeprefix("b'").removesuffix("\\n'")
 
     return data
+
+
+WINDOW.fill((0, 0, 0))
+updated_text = DOWNLOADING_FONT.render("Relaunching game...", True, (255, 255, 255))
+updated_text_rect = updated_text.get_rect()
+WINDOW.blit(updated_text, 
+            (
+                win_centrex-updated_text_rect.width/2, # Xpos
+                win_centrey, # Ypos
+            ),
+            )
+
+pygame.display.flip()
+
+time.sleep(2)
+
+
+pygame.quit()
+args = [sys.executable, f"{os.path.join(DIRPATH, 'main.py')}"]
+subprocess.Popen(args, start_new_session=True)
+exit(0)
